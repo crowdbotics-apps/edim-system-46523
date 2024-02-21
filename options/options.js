033a20a7c8289dd94977dc88c79195e58aab553a
Crowdbotics <team@crowdbotics.com>
2024-02-21 08:06:27 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "edim-system-46523",
+  url: "https://edim-system-46523.botics.co",
+  api: "https://edim-system-46523.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
