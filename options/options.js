10ad85d0c272a0b0bad4f056f4c100dde373339c
Niko Klausnitzer <niko.klausnitzer@develappers.de>
2024-02-22 11:32:32 +0100

    [Crowdbotics Platform Commit]
    Crowdbotics Studio: Update app screens

--- options/options.js
+++ options/options.js
@@ -1,10 +1,11 @@
 export const globalOptions = {
-  name: "edim-system-46523",
-  url: "https://edim-system-46523.botics.co",
-  api: "https://edim-system-46523.botics.co/api/v1",
-  screenOptions: {
-    headerShown: false
-  }
+  "name": "edim-system-46523",
+  "url": "https://edim-system-46523.botics.co",
+  "api": "https://edim-system-46523.botics.co/api/v1",
+  "screenOptions": {
+    "headerShown": false
+  },
+  "initialRoute": "welcome"
 };
 export const modulesOptions = {
   "@modules/app-menu": {
